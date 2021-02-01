f001bd8fa85b8af4258fd5fbdadc21b68b8a9511
dugazada <dugazada@gmail.com>
2021-02-02 00:28:01 +0900

    04-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }){
+  return (<div>
+  <h2>I like {name}</h2>
+  <img src={picture} />
+  </div>
+  );
 }
+
+const foodLike =[
+  {
+    name: 'Kimchi',
+    image:'https://i.pinimg.com/originals/9f/d0/38/9fd038a367a3d54640dce5fa87085c38.png',
+  },
+  {
+    name: 'Samgyeopsal',
+    image:'https://i.pinimg.com/originals/13/9b/68/139b683954c314c5195f1c4228d6dd72.jpg',
+  },
+  {
+    name: 'Doncasu',
+    image:'https://i.pinimg.com/originals/fb/ef/4c/fbef4c995756d660685db9e0a55af39f.jpg',
+  },
+  {
+    name: 'Kimbap',
+    image:'https://i.pinimg.com/originals/44/5c/fb/445cfb0b5452dbe97662e54a1aecf5e9.jpg',
+  },
+];
 
 function App() {
   return (
   <div>
-      <h1>Hello</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="chukumi" />
+      {foodLike.map(dish => (
+      <Food name={dish.name} picture={dish.image} />
+      ))}
   </div>
   );
 }
